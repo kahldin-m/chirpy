cf4f391134d35e3c32084a2ee706a0ebd65dcd0a
kahldin-m <mglen.413@workmail.com>
2026-07-30 09:24:42 -0800

    feat: add health check endpoint and scope static files to /app

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+	
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	serverNew := http.Server{
-		Addr: ":8080",
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+	
+	srv := http.Server{
+		Addr: ":" + port,
 		Handler: mux,
 	}
-	serverNew.ListenAndServe()
 
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
